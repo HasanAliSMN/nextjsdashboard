890da730433bf198424466cd2c4d1f4d008f6c2b
Hasan Ali <hasanalisomuncuoglu@gmail.com>
2024-11-27 21:05:45 +0300

    tested some css

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,9 @@
+import "./Dashboard.module.css"
+
 export default function Page() {
-    return <p>Dashboard Page1</p>;
+    return <main>
+      <div className="hasanali border-dashed border-black border-4 bg-orange-300 p-8 w-72 flex justify-center rounded-lg">
+          <h1 className=" hover:text-sky-200"><strong><em>Dashboard</em></strong></h1>
+      </div>
+    </main>;
   }
