bd82ca810b04a3bca449265767d2ce790d981bd2
Hasan Ali <hasanalisomuncuoglu@gmail.com>
2024-11-30 18:03:52 +0300

    checkout /dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,55 +1,39 @@
-import "./Dashboard.module.css"
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue, 
+         fetchLatestInvoices, 
+         fetchCardData } from '@/app/lib/data';
+ 
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
 
-export default function Page() {
-  const singleDigit = [1, 6, 5, 2];
-  const numbers = [3456, 24589037, 2, 12, 23545234, 2967, 1238, 1092370, 12, 1];
-
-    return <main>
-      <div className="hasanali border-dashed border-black border-4 bg-orange-300 p-8 w-72 flex flex-col justify-center rounded-lg text-center">
-          <h1 className="bg-slate-400 hover:text-sky-200"><strong><em>Dashboard</em></strong></h1>
-          <div>
-          <ul className="bg-green-400 text-left">
-              {basamak(numbers).map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-          </ul>
-          </div>
+  const data = fetchCardData();
+  const totalPaidInvoices = (await data).totalPaidInvoices;
+  const totalPendingInvoices = (await data).totalPendingInvoices;
+  const numberOfInvoices = (await data).numberOfInvoices;
+  const numberOfCustomers = (await data).numberOfCustomers;
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {<Card title="Collected" value={totalPaidInvoices} type="collected" />}
+        {<Card title="Pending" value={totalPendingInvoices} type="pending" />}
+        {<Card title="Total Invoices" value={numberOfInvoices} type="invoices" />}
+        {<Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />}
       </div>
-    </main>;
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue}  />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
+  );
 }
-
-/*MAYBE TODO:
-    this sends user an array containing parsed strings
-    send like this:
-    {
-      ["3x100", "5x10", "1"]             
-    }
-      maybe this would be a better aproach
-*/
-function basamak(arr: number[]): string[]{
-  const retVal: string[] = [];
-  for (let i = 0; i < arr.length; i++) {
-    //for every elemnt find the val and push to retVal
-    const val: string = arr[i].toString();
-    let basamak: string = onlar(val);
-    retVal.push(basamak);
-  }
-  console.log(retVal);
-  return retVal;
-}
-function onlar(str: string): string{
-  let retVal: string = "";
-  if(str.length == 1) return str + "x1";
-  for (let i = 0; i < str.length; i++) {
-      /* 0-1-2-3 */
-     retVal = retVal + str.charAt(i) + "x" + amk(str.length - i - 1);
-  }
-  return retVal;
-}
-function amk(num: number): string{
-  let retVal: string = "1";
-  for (let i = 0; i < num; i++) {
-    retVal += "0"; 
-  }
-  return retVal  + " ";
-} 
